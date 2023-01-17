3f9f6f982f4ae89e2892f73cbdb5d6631e8f8dff
Pradeepchandra Reddy S C <pradeepchandrareddy95@gmail.com>
2023-01-17 14:56:31 +0530

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -1,1 +1,16 @@
-SELECT
+/*1- write a sql to get all the orders where customers name has "a" as second character and "d" as fourth character (58 rows)*/
+
+SELECT 
+    customer_name
+FROM
+    orders
+WHERE
+    customer_name LIKE '_a%'
+        AND customer_name LIKE '___d%'; 
+        
+SELECT 
+    customer_name
+FROM
+    orders
+WHERE
+    customer_name LIKE '_a_d%';

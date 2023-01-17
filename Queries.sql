00ca8dca954f612e032cce8154ed262e625d268c
Pradeepchandra Reddy S C <pradeepchandrareddy95@gmail.com>
2023-01-17 15:39:29 +0530

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -80,3 +80,13 @@ WHERE
     category = 'Furniture'
 order by sales desc
 LIMIT 5; 
+
+/*9- write a query to find all the records in technology and furniture category for the orders placed in the year 2020 only*/
+
+SELECT 
+    *
+FROM
+    orders
+WHERE
+    category IN ('Technology' , 'Furniture')
+        AND order_date BETWEEN '01-01-2020' AND '31-12-2020';

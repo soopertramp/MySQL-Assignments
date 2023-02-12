40180f015bd40583af3508992aacac364c369dd8
soopertramp <pradeepchandrareddy95@gmail.com>
2023-02-12 13:04:41 +0530

    update

--- Queries.sql
+++ Queries.sql
@@ -622,3 +622,18 @@ SELECT
   COUNT(*) - SUM(win_flag) AS matches_lost
 FROM all_teams
 GROUP BY team;
+
+/*42- write a query to print first name and last name of a customer using orders table(everything after first space can be considered 
+as last name) customer_name, first_name,last_name*/
+
+select * from orders;
+
+SELECT 
+	customer_name,
+    SUBSTRING(customer_name,
+        1,
+        LOCATE(' ', customer_name) - 1) AS first_name,
+    SUBSTRING(customer_name,
+        LOCATE(' ', customer_name) + 1) AS last_name
+FROM
+    orders;
